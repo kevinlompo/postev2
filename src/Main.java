4e4d7fa38141e611fdf56d86201961b3785ab926
kevin <kevinlompo12@gmail.com>
2022-10-12 15:05:20 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 /*        Colis colis1 = new Colis("Vincent", "Av. Berthelot", "Paris", 75005, false, 0.741);
         Colis colis2 = new Colis("Dubois", "Bd Victor Hugo", "Toulon", 83000, true, 1.387);
         Lettre lettre1 = new Lettre("Dupont", "Rue Colbert", "Marseille", 13001, false, true);
         Lettre lettre2 = new Lettre("Durand", "Rue Joloit-Cuire", "Marseille", 13013, true, false);*/
 
-        ObjetPostal [] BoiteALettres = new ObjetPostal[4];
+        List<ObjetPostal> BoiteALettres = new ArrayList<ObjetPostal>();
 
-        for (int i = 0; i<BoiteALettres.length; i++) {
-            System.out.println("\n" + "** Objet no " + i + " : ");
-            BoiteALettres[i] = menu();
-            System.out.println("\n" + BoiteALettres[i]);
+        for (int i = 0; i<=3; i++) {
+            System.out.println("\n" + "** Objet n° " + i + " : ");
+            BoiteALettres.add(menu());
+            System.out.println("\n" + BoiteALettres.get(i).toString());
         }
     }
-    public static ObjetPostal menu() {
+
+    public static ObjetPostal menu() throws IOException  {
         Lettre lettre = null;
         Colis colis = null;
         Scanner sc = new Scanner(System.in);
-        int swValue;
+        String swValue = "";
         System.out.println("============================");
         System.out.println("|   MENU DE SELECTION       |");
         System.out.println("============================");
@@ -28,50 +37,58 @@ public class Main {
         System.out.println("|        2. COLIS           |");
         System.out.println("|        3. Exit            |");
         System.out.println("============================");
-        swValue = sc.nextInt();
+        swValue = sc.nextLine();
 
-        switch (swValue) {
-            case 1:
-                lettre = lettreInfo();
-                break;
-            case 2:
-                colis = colisInfo();
-                break;
-            case 3:
-                System.out.println("Exit selected");
-                System.exit(0);
-            default:
-                System.out.println("Invalid selection");
-                break;
-            // This break is not really necessary
+        while(!swValue.matches("[1-3]")) {
+            System.out.println("sois 1 2 3");
+            menu();
         }
-        if (swValue == 1) {
+        if (swValue.equals("1")){
+            lettre = lettreInfo();
             return lettre;
         }
-        else if(swValue == 2) {
+        else if(swValue == "2") {
+            colis = colisInfo();
             return colis;
         }
-        else return null;
+        else if(swValue == "3") {
+            System.out.println("Exit selected");
+            System.exit(0);
+        }
+
+        return null;
     }
     public static Lettre lettreInfo() {
         Scanner sc = new Scanner(System.in);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+       String temp = bf.readLine();
+       int tempI = bf.read();
+       String value ="";
+       boolean tempB = bf.ready();
         Lettre lettre = new Lettre("", "", "", 00000, false, true);
-        System.out.print("Entrer le nom du destinataire de la lettre : " );
-        lettre.setNom(sc.nextLine());
+        System.out.print("Entrer le nom du destinataire de la lettre : ");
+        lettre.setNom(temp);
         System.out.print("Entrer l'adresse du destinataire de la lettre : ");
-        lettre.setAdresse(sc.nextLine());
+        lettre.setAdresse(temp);
         System.out.print("Entrer la ville du destinataire de la lettre : ");
-        lettre.setNomVille(sc.nextLine());
-        System.out.print("Entrer le code postal du destinataire de la lettre ");
-        lettre.setCodePost(sc.nextInt());
-        System.out.print("Est-elle en recommandee ? " );
-        lettre.setExRec(sc.nextBoolean());
+        lettre.setNomVille(temp);
+        System.out.print("Entrer le code postal du destinataire de la lettre :  ");
+        lettre.setCodePost(tempI);
+        System.out.print("Est-elle en recommandee ? (1 pour oui et 2 pour non)");
+        try {
+            lettre.setExRec(tempB);
+        } catch (NumberFormatException e) {
+            while (temp.matches("[1-2]")) {
+                value = sc.nextLine();
+                lettre.exRec = value == "1" ? true : false;
+            }
+        }
         System.out.print("Est-elle en urgence ? ");
         lettre.setExUr(sc.nextBoolean());
         lettre.prixAff();
         return lettre;
     }
-    public static Colis colisInfo() {
+    public static Colis colisInfo() throws IOException {
         Scanner sc = new Scanner(System.in);
         Colis colis = new Colis("", "", "", 00000, false, 0);
         System.out.print("Entrer le nom du destinataire du colis : " );
@@ -89,4 +106,5 @@ public class Main {
         colis.prixAff();
         return colis;
     }
-}
+
+    }
